c2779db8959325c1fb688789304cc5eca932d1ec
Egor Oshchepkov <oschepkov.egor55@gmail.com>
2025-02-22 19:59:43 +0600

    feat: reinit project

--- src/shared/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-
-export const theme = responsiveFontSizes(
-  createTheme({
-    typography: {
-      htmlFontSize: 16,
-    },
-  })
-);
